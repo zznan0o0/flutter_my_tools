886d547b82f4f848f126f54f3b8ff38a8465a547
zzn <you@example.com>
2024-09-05 16:23:33 +0800

    解决order by 添加in语句转换

--- lib/page/mybatis_where_tool_page.dart
+++ lib/page/mybatis_where_tool_page.dart
@@ -17,7 +17,8 @@ class MybatisWhereToolPage extends StatelessWidget{
     for (int i = 1; i < words.length; i++) {
       words[i] = words[i].substring(0, 1).toUpperCase() + words[i].substring(1);
     }
-    return words.join('');
+    String result = words.join('');
+    return result.substring(0, 1).toLowerCase() + result.substring(1);
   }
 
   void _copy() async{
@@ -53,8 +54,8 @@ class MybatisWhereToolPage extends StatelessWidget{
       else{
         List<String> spByOrder = sqlWhere.split(orderbyText);
         if(spByOrder.length > 1){
-          sqlWhere = spByGroup[1];
-          afterString = orderbyText + spByOrder[2];
+          sqlWhere = spByOrder[0];
+          afterString = orderbyText + spByOrder[1];
         }
       }
       String andSp = "and";
@@ -72,18 +73,29 @@ class MybatisWhereToolPage extends StatelessWidget{
         'NOT LIKE',
         'like',
         'LIKE',
-        // 'not in',
-        // 'NOT IN',
-        // 'in',
-        // 'IN',
+        'not in',
+        'NOT IN',
+        'in',
+        'IN',
+      ];
+
+      List<String> inSymbols = [
+        'not in',
+        'NOT IN',
+        'in',
+        'IN',
       ];
       int i = 0;
       for(String andItem in andStrs){
         i++;
         String spStr = "";
+        bool inFlag = false;
         for(String ws in whereSymbols){
           if(andItem.contains(ws)){
             spStr = ws;
+            if(inSymbols.contains(ws)){
+              inFlag = true;
+            }
             break;
           }
         }
@@ -93,16 +105,39 @@ class MybatisWhereToolPage extends StatelessWidget{
         }
         List<String> fileds = andItem.split(spStr);
         String fieldName = fileds[0].trim();
-        whereResults.add('''\n<if test="$fieldName != null and $fieldName != ''">${i > 1 ? '$andSp ' : ''}$fieldName$spStr#{${toLowerCamelCase(fieldName)}}</if>''');
+        String lowerCamelCaseFieldName = toLowerCamelCase(fieldName);
+        if(inFlag){
+          whereResults.add('''\n<if test="$lowerCamelCaseFieldName != null and $lowerCamelCaseFieldName.size > 0">${i > 1 ? '$andSp ' : ''}$fieldName $spStr <foreach collection="$lowerCamelCaseFieldName" item="itemId" index="index" open="(" close=")" separator=",">#{itemId}</foreach></if>''');
+        }
+        else{
+          whereResults.add('''\n<if test="$lowerCamelCaseFieldName != null and $lowerCamelCaseFieldName != ''">${i > 1 ? '$andSp ' : ''}$fieldName$spStr#{$lowerCamelCaseFieldName}</if>''');
+        }
         // String fieldStr = andItem.split(spStr);
       }
       whereString = whereResults.join('');
+      if(afterString.isNotEmpty){
+        afterString = _replaceFirstEnglishCharWithNewline(afterString);
+      }
       controller.setResult('$beforeString$whereString$afterString');
     }
     else{
       controller.setResult(sql);
     }
   }
+
+  /// 使用正则表达式替换第一个英文字符之前的内容为换行符
+String _replaceFirstEnglishCharWithNewline(String str) {
+  // 正则表达式：匹配第一个英文字符之前的内容
+  final regex = RegExp(r'^[^a-zA-Z]*([a-zA-Z])');
+  final match = regex.firstMatch(str);
+
+  if (match != null) {
+    final index = match.start + match.group(0)!.length - 1;
+    return '\n${str.substring(index)}';
+  } else {
+    return str; // 未找到英文字符，返回原字符串
+  }
+}
 
   @override
   Widget build(BuildContext context) {
